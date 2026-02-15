95d89567c68c59cde0295ab9288f8c964a9761d7
Aditia Andrian <aditiaandrian43@gmail.com>
2026-02-15 12:22:53 +0700

    Add preinstall for python & ffmpeg

--- .railway/preinstall.sh
+++ .railway/preinstall.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-# Update repo dan install python + ffmpeg
+# Update dan install python + ffmpeg
 apt-get update
 apt-get install -y python3 python3-pip ffmpeg libatomic1
 
-# Buat python3 sebagai default
+# Buat python3 default
 ln -sf /usr/bin/python3 /usr/bin/python
