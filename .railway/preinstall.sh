9f94f21700e093c59c25d60afb46eec64c6e1c1b
Aditia Andrian <aditiaandrian43@gmail.com>
2026-02-15 12:29:06 +0700

    Make preinstall.sh executable with LF line endings

--- .railway/preinstall.sh
+++ .railway/preinstall.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -e
 
-# Update dan install python + ffmpeg
+echo "=== RUNNING PREINSTALL ==="
+
 apt-get update
 apt-get install -y python3 python3-pip ffmpeg libatomic1
 
-# Buat python3 default
 ln -sf /usr/bin/python3 /usr/bin/python
+
+echo "=== PYTHON INSTALLED ==="
+python --version
